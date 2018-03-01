370aa0ce83932a12c53255ff481ab4f4cbb4c771
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2018-03-01 17:08:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+var result;
+if(str.length%2)
+result = false;
+else
+{
+var a,b,c,last,q, stack = [];
+for(var i=0; i<str.length; i++){
+    q = str[i];
+    if(q == '(' || q == '[' ||  q == '{' || q == '1' || q == '3' || q == '5' )
+    {
+        stack.push(q);
+        last = q;
+
+        }
+     else if(q == ')' || q == ']' || q == '}' || q == '2' || q == '4' || q == '6' )
+     {
+     if(last)
+     {
+     if(q == '(' && last == ')' || q == '[' && last == ']' || q == '{' && last == '}' || q == '1' && last == '2' || q == '3' && last == '4' || q == '5' && last == '6')
+     {
+
+     stack.pop();
+     last = stack.length > 0 ? stack[stack.length - 1] : undefined;
+
+        }
+
+        }else
+        result = false;
+        }
 }
+
+result = true;
+
+
+}
+return result;
+}
+
