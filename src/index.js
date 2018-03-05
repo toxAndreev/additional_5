e43c14d802c15c52e22a89a8c31b162bf46c7ec9
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2018-03-05 16:03:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var result;
+	if (str.length % 2 !== 0)
+    result = false;
+	while (true) {
+		var oldlength = str.length;
+		for (var i = 0; i < bracketsConfig.length; i++) {
+			str = str.replace(bracketsConfig[i].join(''), '');
+		}
+		if (str.length === oldlength || str.length === 0) {
+			break;
+		}
+	}
+  if(str.length === 0)
+  result = true;
+  else
+    result = false;
+	return result;
 }
