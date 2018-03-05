e9dce45618365fb66c2c41a4eeebf6143706c608
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2018-03-05 16:07:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-var result;
-if(str.length%2)
-result = false;
-else
-{
-var a,b,c,last,q, stack = [];
-for(var i=0; i<str.length; i++){
-    q = str[i];
-    if(q == '(' || q == '[' ||  q == '{' || q == '1' || q == '3' || q == '5' )
-    {
-        stack.push(q);
-        last = q;
-
-        }
-     else if(q == ')' || q == ']' || q == '}' || q == '2' || q == '4' || q == '6' )
-     {
-     if(last)
-     {
-     if(q == '(' && last == ')' || q == '[' && last == ']' || q == '{' && last == '}' || q == '1' && last == '2' || q == '3' && last == '4' || q == '5' && last == '6')
-     {
-
-     stack.pop();
-     last = stack.length > 0 ? stack[stack.length - 1] : undefined;
-
-        }
-
-        }else
+	var result;
+	if (str.length % 2 !== 0)
         result = false;
-        }
+	while (1) {
+		var oldlength = str.length;
+		for (var i = 0; i < bracketsConfig.length; i++) {
+			str = str.replace(bracketsConfig[i].join(''), '');
+		}
+		if (str.length === oldlength || str.length === 0) {
+			break;
+		}
+	}
+    if(str.length === 0)
+        result = true;
+    else 
+        result = false;
+	return result;
 }
-
-result = true;
-
-
-}
-return result;
-}
-
